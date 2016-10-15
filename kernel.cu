2929b29004b38e05620044a42e48f9885060bf7c
oreqizer <oreqizer@gmail.com>
2016-10-15 21:30:44 +0200

    kernel: Adjust stuff

--- kernel.cu
+++ kernel.cu
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
+#define BLOCK_SIZE = 512
 
 __global__ void solver(
     const int *results,
     float *avg_stud,
     float *avg_que,
-    const int dim_x,  // always divisible by 32
-    const int dim_y  // always divisible by 32
+    const int dim_x,
+    const int dim_y
 ) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     int resultIds = y*dim_x + x;
 
-    // add to resulting array
+    // add to resulting arrays
     avg_que[x] += results[resultIds];
     avg_stud[y] += results[resultIds];
 }
@@ -24,7 +25,12 @@ void solveGPU(
     const int students,  // y: always divisible by 32
     const int questions  // x: always divisible by 32
 ) {
-    solver<<<questions, students>>>(
+    int n = students * questions;
+    solver<<<n, n / BLOCK_SIZE>>>(
         results, avg_stud, avg_que, questions, students
     );
+
+    if (cudaPeekAtLastError() != cudaSuccess) {
+        printf("Error: %s\n", cudaGetErrorString(cudaGetLastError()));
+    }
 }
