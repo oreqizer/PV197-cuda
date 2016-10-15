12368d7ad691a26a4db2a085978dfd00b442ede6
oreqizer <oreqizer@gmail.com>
2016-10-15 19:39:08 +0200

    kernel: Basic

---
+++ copy.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+
+scp framework.cu kernel_CPU.C kernel.cu 433689@airacuda.fi.muni.cz:~/cuda
--- kernel.cu
+++ kernel.cu
@@ -1,11 +1,30 @@
-//XXX write kernel codes here
+#include <stdio.h>
 
-void solveGPU(
+
+__global__ void solver(
     const int *results,
     float *avg_stud,
     float *avg_que,
-    const int students,
-    const int questions
+    const int dim_x,  // always divisible by 32
+    const int dim_y  // always divisible by 32
 ) {
-    //XXX write kernel calls here
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int resultIds = y*dim_x + x;
+
+    // add to resulting array
+    avg_que[x] += results[resultIds];
+    avg_stud[y] += results[resultIds];
+}
+
+void solveGPU(
+    const int *results,  // students * questions
+    float *avg_stud,
+    float *avg_que,
+    const int students,  // y: always divisible by 32
+    const int questions  // x: always divisible by 32
+) {
+    solver<<<questions, students>>>(
+        results, avg_stud, avg_que, questions, students
+    );
 }
