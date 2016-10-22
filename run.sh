25c7ab2121c774e761434ef7b3e4dbf1c141f0ba
oreqizer <oreqizer@gmail.com>
2016-10-22 16:01:11 +0200

    run: Niceify

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 
 HOST=433689@airacuda.fi.muni.cz
 scp framework.cu kernel_CPU.C kernel.cu $HOST:~/cuda
-ssh $HOST 'cd ~/cuda && /usr/local/cuda/bin/nvcc -o run -arch=compute_30 -code=sm_30 framework.cu; ~/cuda/run; rm ~/cuda/run'
+ssh $HOST "cd ~/cuda;\
+/usr/local/cuda/bin/nvcc -o run -arch=compute_30 -code=sm_30 framework.cu;\
+~/cuda/run;\
+rm ~/cuda/run"
