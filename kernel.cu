548dc7edefef2185a209981fd4e0ba9c9efbba0d
oreqizer <oreqizer@gmail.com>
2016-11-15 14:23:01 +0100

    kernel: Start verticalizing

--- kernel.cu
+++ kernel.cu
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 #define N      32       // block rows, tile side
-#define C      (N / 4)  // block columns
 #define BLOCKS (N * N)  // total blocks
 
-template <int T>
+template<int T>
 __global__ void reduce(const int *in, float *out_stud, float *out_que) {
     __shared__ int tile[T * (T + 1)];
-    int x4 = blockIdx.x*(T / 4) + threadIdx.x;
-    int y  = blockIdx.y* T      + threadIdx.y;
-    int width = gridDim.x*(T / 4);
+    __shared__ int stud_res[T*4];  // TODO make it iterate 8 * 128
+
+    int x4 = blockIdx.x*(T/4) + threadIdx.x;
+    int y  = blockIdx.y* T    + threadIdx.y;
+    int width = gridDim.x*(T/4);
 
     int4 val = reinterpret_cast<const int4*>(in)[y*width + x4];
-    reinterpret_cast<int4*>(tile)[(threadIdx.y*(T / 4)) + threadIdx.x] = val;
+    reinterpret_cast<int4*>(tile)[(threadIdx.y*(T/4)) + threadIdx.x] = val;
     __syncthreads();
 
     if (threadIdx.x == 0) {
@@ -21,9 +22,9 @@ __global__ void reduce(const int *in, float *out_stud, float *out_que) {
             sum += tile[threadIdx.y*T + i];
         }
         atomicAdd(out_stud + y, sum);
-    } else if (threadIdx.x == 1) {
+
         int x = blockIdx.x*T + threadIdx.y;
-        int sum = 0;
+        sum = 0;
         for (int i = 0; i < T; i++) {
             sum += tile[i*T + threadIdx.y];
         }
@@ -51,7 +52,7 @@ void solveGPU(
     cudaMemset(avg_stud, 0, Y*sizeof(avg_stud[0]));
     cudaMemset(avg_que, 0, X*sizeof(avg_que[0]));
 
-    dim3 blocks(X/N, Y/N);
+    dim3 blocks(X/N, Y/(N*4));
     dim3 threads(N/4, N);
     reduce<N><<<blocks, threads>>>(results, avg_stud, avg_que);
 
--- profile.sh
+++ profile.sh
@@ -4,4 +4,5 @@ HOST=433689@airacuda.fi.muni.cz
 scp framework.cu kernel_CPU.C kernel.cu $HOST:~/cuda
 ssh $HOST "cd ~/cuda &&\
 /usr/local/cuda/bin/nvcc -o run -arch=compute_30 -code=sm_30 framework.cu;\
-/usr/local/cuda/bin/nvprof --print-gpu-trace ./run"
+/usr/local/cuda/bin/nvprof --log-file logs.log ./run;\
+cat logs.log"
